200ca9c497b4f9e5fd2c2999cbd1c7e997c07018
rociosanchez <sanchez.fernandez.rocio.ald@gmail.com>
2023-03-06 20:03:28 +0100

    Añadido método Se acabó

--- LightsOut.java
+++ LightsOut.java
@@ -48,4 +48,15 @@ import java.util.Scanner;
 	        }
 	    }
 
+		private boolean seAcabo() {
+	        for (int i = 0; i < size; i++) {
+	            for (int j = 0; j < size; j++) {
+	                if (lights[i][j]) {
+	                    return false;
+	                }
+	            }
+	        }
+	        return true;
+	    }
+
     }
