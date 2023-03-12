4df35d123e62f93a31f438bad1e4b79233860115
Manuel Durán Sueiro <duran.sueiro.manu.ald@gmail.com>
2023-03-12 15:16:16 +0100

    Implementación de la impresión del tablero y el main realizada

--- LightsOut.java
+++ LightsOut.java
@@ -59,12 +59,20 @@ import java.util.Scanner;
 	        return true;
 	    }
 
-		private void imprimirTablero(){
 
-		}
+        private void imprimirTablero() {
+	        for (int i = 0; i < size; i++) {
+	            for (int j = 0; j < size; j++) {
+	                System.out.print(lights[i][j] ? "1 " : "0 ");
+	            }
+	            System.out.println();
+	        }
+	    }
 
-		public static void main(String [] args){
-			
-		}
+	    public static void main(String[] args) {
+	        new LightsOut().jugar();
+	    }
+
+
 
     }
